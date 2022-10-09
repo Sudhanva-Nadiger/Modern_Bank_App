acf74710789c0f9d72815f3aa1290eeacb6239e9
Sudhanva-Nadiger <sudhanvanadiger12@gmail.com>
2022-10-09 18:35:09 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import styles from './style'
+
+import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
+
+const App = () => {
+  return (
+    <div className='bg-primary w-full overflow-hodden'>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+
+      </div>
+      <div className={`bg-primary ${styles.flexStart} ${styles.paddingX}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import styles from '../style'
+import { logo } from '../assets'
+import { footerLinks, socialMedia } from '../constants'
+
+const currYear = new Date().getFullYear();
+
+const Footer = () => {
+    return (
+        <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`} >
+            <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`} >
+                <div className='flex-1 flex flex-col justify-start mr-10'>
+                    <img src={logo} alt="hooBank" className='w-[266px] h-[72px object-contain' />
+                    <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>
+                        A new way to make the payments easy, reliable and secure.
+                    </p>
+                </div>
+
+                <div className='flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10'>
+                    {
+                        footerLinks.map((footerLisnk) => {
+                            return (
+                                <div key={footerLisnk.key}
+                                    className="flex flex-col ss:my-0 my-4 min-w-[150px]"
+                                >
+                                    <h4 className='font-poppins font-medium text-[18px] leading-[27px] text-white' >
+                                        {footerLisnk.title}
+                                    </h4>
+                                    <ul className='list-none mt-4'>
+                                        {
+                                            footerLisnk.links.map((link, index) => {
+                                                return (
+                                                    <li
+                                                        key={link.name}
+                                                        className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${index !== footerLisnk.links.length ? "mb-4" : "mb-0"}`}
+                                                    >
+                                                        {link.name}
+
+                                                    </li>
+                                                )
+                                            })
+                                        }
+
+                                    </ul>
+
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+
+            <div className='w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3f3e45]'>
+                <p className='font-poppins font-normal text-center text-[18px] leading-[27px] text-white'>
+                    {currYear} HooBank All Rights Reserved
+                </p>
+                <div className="flex flex-row md:mt-0 mt-6">
+                    {socialMedia.map((social, index) => (
+                        <img
+                            key={social.id}
+                            src={social.icon}
+                            alt={social.id}
+                            className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
+                                }`}
+                            onClick={() => window.open(social.link)}
+                        />
+                    ))}
+                </div>
+            </div>
+
+
+        </section>
+
+    )
+}
+
+export default Footer
